a91759502cf2e8ebfc8571df040d60caffb8bf1b
Diego Alencar Jacober <57761933898>
2023-09-13 16:36:02 -0300

    Creating tables

--- models/__all_models.py
+++ models/__all_models.py
@@ -1,1 +1,5 @@
 from models.user_model import UserModel
+from models.country_model import CountryModel
+from models.league_model import LeagueModel
+from models.season_model import SeasonModel
+from models.team_model import TeamModel
