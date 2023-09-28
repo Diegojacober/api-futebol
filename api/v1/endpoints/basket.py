7ed5eee98385e3008d01a20e185dd209bbcf2821
ETS <ct67ca@bosch.com>
2023-09-28 11:27:26 -0300

    Baskt api new endpoint

--- api/v1/endpoints/basket.py
+++ api/v1/endpoints/basket.py
@@ -11,8 +11,19 @@ router = APIRouter()
 @router.get('/teams')
 async def get_teams():
     async with httpx.AsyncClient() as client:
-        r = await client.get(f'http://10.21.62.8:8001/api/v1/basquete/teams')
+        r = await client.get(f'http://10.21.62.8:8011/api/v1/basquete/teams')
         
         response = r.json()
         
         return response
+
+# GET leagues 
+@router.get('/leagues')
+async def get_leagues():
+    async with httpx.AsyncClient() as client:
+        e = await client.get(f'http://10.21.62.8:8011/api/v1/basquete/leagues')
+        
+        response = e.json()
+        
+        return response
+    
